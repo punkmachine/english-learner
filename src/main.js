b528796741a752cacfab36f2f2828c4bc63a5b01
punkmachine <alex_rassydihin@mail.ru>
2022-10-08 19:38:27 +0600

    add vue axios.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,23 @@
 import { createApp } from 'vue';
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 import App from './App.vue';
+
 import router from './router';
 import store from './store';
+
 import vuetify from './plugins/vuetify';
 import { loadFonts } from './plugins/webfontloader';
 
 loadFonts()
 
-createApp(App)
-	.use(router)
+const app = createApp(App);
+
+app.use(router)
 	.use(store)
 	.use(vuetify)
+	.use(VueAxios, axios)
+	.provide('axios', app.config.globalProperties.axios)
 	.mount('#app');
