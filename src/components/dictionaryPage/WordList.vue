1f9f17b6309d626675fd67c2e81b8389a1b14c9b
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2022-10-09 17:21:03 +0600

    Выделил компонент списка слов.

---
+++ src/components/dictionaryPage/WordList.vue
@@ -0,0 +1,50 @@
+<template>
+	<tbody>
+		<!-- TODO: список отображать в lazy -->
+		<tr
+			v-for="(word, index) in wordList"
+			:key="word.id"
+			:class="{'dictionary-page__tr-bg': index % 2 === 0}"
+		>
+			<td> {{ index + 1 }} </td>
+			<!-- TODO: сделать слова с больших букв -->
+			<td> {{ word['word_variants_ru'].join(', ') }} </td>
+			<td> {{ word['word_variants_en'].join(', ') }} </td>
+			<td>
+				<div class="dictionary-page__btn-group d-flex">
+					<v-btn
+						@click="editWordClick(word.id)"
+						icon="mdi-pencil"
+						class="el-btn el-text-white"
+						size="x-small"
+					/>
+					<v-btn
+						@click="deleteWord(word.id)"
+						icon="mdi-delete"
+						class="el-btn el-text-white"
+						size="x-small"
+					/>
+				</div>
+			</td>
+		</tr>
+	</tbody>
+</template>
+
+<script>
+export default {
+	props: {
+		wordList: {
+			required: true,
+			type: Array,
+		}
+	},
+	setup(props, { emit }) {
+		const editWordClick = (id) => emit('editWordClick', id);
+		const deleteWord = (id) => emit('deleteWord', id);
+
+		return {
+			editWordClick, deleteWord
+		}
+	}
+};
+</script>
--- src/pages/DictionaryPage.vue
+++ src/pages/DictionaryPage.vue
@@ -32,35 +32,11 @@
 				</template>
 
 				<template #tbody>
-					<tbody>
-						<!-- TODO: список отображать в lazy -->
-						<tr
-							v-for="(word, index) in wordList"
-							:key="word.id"
-							:class="{'dictionary-page__tr-bg': index % 2 === 0}"
-						>
-							<td> {{ index + 1 }} </td>
-							<!-- TODO: сделать слова с больших букв -->
-							<td> {{ word['word_variants_ru'].join(', ') }} </td>
-							<td> {{ word['word_variants_en'].join(', ') }} </td>
-							<td>
-								<div class="dictionary-page__btn-group d-flex">
-									<v-btn
-										@click="editWordClick(word.id)"
-										icon="mdi-pencil"
-										class="el-btn el-text-white"
-										size="x-small"
-									/>
-									<v-btn
-										@click="deleteWord(word.id)"
-										icon="mdi-delete"
-										class="el-btn el-text-white"
-										size="x-small"
-									/>
-								</div>
-							</td>
-						</tr>
-					</tbody>
+					<WordList
+						:wordList="wordList"
+						@editWordClick="editWordClick"
+						@deleteWord="deleteWord"
+					/>
 				</template>
 
 				<template #tfoot>
@@ -273,8 +249,9 @@
 </template>
 
 <script>
-import SearchWords from '@/components/dictionaryPage/SearchWord.vue';
 import TableWrapper from '@/components/shared/TableWrapper.vue';
+import SearchWords from '@/components/dictionaryPage/SearchWord.vue';
+import WordList from '@/components/dictionaryPage/WordList.vue';
 
 export default {
 	data() {
@@ -297,7 +274,7 @@ export default {
 		}
 	},
 	components: {
-		SearchWords, TableWrapper
+		SearchWords, TableWrapper, WordList
 	},
 	mounted() {
 		this.getWordsList();
