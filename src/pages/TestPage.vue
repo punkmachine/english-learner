b40d10d3ef2ff7416f939b0631dccb5217cf2b0e
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2022-10-10 19:46:22 +0600

    Добавляет базовое прохождение теста до финала.

--- src/pages/TestPage.vue
+++ src/pages/TestPage.vue
@@ -4,27 +4,28 @@
 			v-if="isCreateTest"
 			class="test-page__create"
 		>
-			<v-card
-				min-width="350px"
-			>
+			<v-card min-width="350px">
 				<v-card-title class="py-2 px-4">Настройки теста</v-card-title>
 				<v-divider />
 
 				<v-card-text class="pa-4">
 					<v-select
 						v-model="countWordsInTest"
-						:items="[10, 20, 25, 50, 100]"
+						:items="[3, 10, 20, 25, 50, 100]"
 						label="Выберите количество слов в тесте"
 					/>
 					<v-select
 						v-model="modeTest"
-						:items="['Вперемешку', 'С русского на английский', 'С английского на русский']"
+						:items="modeTestItems"
+						item-title="label"
+						item-value="id"
 						label="Выберите режим перевода"
 					/>
 				</v-card-text>
 
 				<v-card-actions>
 					<v-btn
+						@click="startTest"
 						class="el-btn el-text-white w-50"
 						block
 						append-icon="mdi-arrow-right"
@@ -34,15 +35,18 @@
 				</v-card-actions>
 			</v-card>
 		</div>
+
 		<div
-			v-else
+			v-else-if="!isFinishTest"
 			class="test-page py-5 px-5"
 		>
 			<div class="test-page__col">
 				<v-card>
-					<v-card-title>Переведите слово: well</v-card-title>
+					<v-card-title>Переведите слово: {{ getWord() }} </v-card-title>
 					<v-card-text>
 						<v-text-field
+							v-model.trim="currentWord"
+							@keypress.enter="currentWordIndex < countWordsInTest - 1 ? nextWord() : doneTest()"
 							label="Слово на русском..."
 						/>
 					</v-card-text>
@@ -50,14 +54,29 @@
 						<v-btn
 							class="el-btn el-text-white"
 							prepend-icon="mdi-arrow-left-bold"
+							:disabled="currentWordIndex === 0"
 						>
 							Пред.
 						</v-btn>
+
 						<v-btn
+							v-if="currentWordIndex < countWordsInTest - 1"
+							@click="nextWord"
+							:disabled="!currentWord"
 							class="el-btn el-text-white"
 							append-icon="mdi-arrow-right-bold"
 						>
 							След.
+						</v-btn>
+
+						<v-btn
+							v-else
+							@click="doneTest"
+							:disabled="!currentWord"
+							class="el-btn el-text-white"
+							append-icon="mdi-check"
+						>
+							Готово
 						</v-btn>
 					</v-card-actions>
 				</v-card>
@@ -66,7 +85,7 @@
 				<div class="test-page__cards">
 					<v-card class="py-3 px-3">
 						<v-card-title class="d-flex align-center gap-10">
-							<span class="statistics-page__cards-title">0/30</span>
+							<span class="statistics-page__cards-title"> {{ currentWordIndex }}/{{ countWordsInTest }} </span>
 						</v-card-title>
 						<v-card-text>Пройдено</v-card-text>
 					</v-card>
@@ -85,22 +104,101 @@
 				</div>
 			</div>
 		</div>
+
+		<div
+			v-else
+		>
+			Finish
+		</div>
 	</div>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, inject } from 'vue';
 
 export default {
 	setup(props, context) {
+		const axios = inject('axios');
+
+		const modeTestItems = [
+			{ id: 1, label: 'Вперемешку' },
+			{ id: 2, label: 'С английского на русский' },
+			{ id: 3, label: 'С русского на английский' },
+		];
+
 		const isCreateTest = ref(true);
-		const modeTest = ref('С английского на русский');
-		const countWordsInTest = ref(25);
+		const modeTest = ref(2);
+		const countWordsInTest = ref(3);
+		const isFinishTest = ref(false);
+
+		const testData = ref([]);
+		// TODO: loader
+		const loading = ref(false);
+
+		const currentWord = ref('');
+		const currentWordIndex = ref(0);
+
+		let requestTest = [];
+
+		function startTest() {
+			loading.value = true;
+
+			// TODO: нормальный catch
+			axios
+				.get(`${process.env.VUE_APP_API_URL}/get-test?wordsAmount=${countWordsInTest.value}`)
+				.then((response) => {
+					testData.value = [ ...response.data ];
+					isCreateTest.value = false;
+				});
+
+			loading.value = false;
+		}
+
+		function getWord() {
+			if (modeTest.value === 2) {
+				return testData.value[currentWordIndex.value]['word_variants_en'][0];
+			} else if (modeTest.value === 1) {
+				return testData.value[currentWordIndex.value]['word_variants_ru'][0];
+			} else {
+				const random = Math.floor(Math.random()*2 + 1) - 1;
+
+				if (random === 1) {
+					return testData.value[currentWordIndex.value]['word_variants_en'][0];
+				} else {
+					return testData.value[currentWordIndex.value]['word_variants_ru'][0];
+				}
+			}
+		}
+
+		function nextWord() {
+			requestTest.push(currentWord.value);
+			currentWord.value = '';
+
+			if (currentWordIndex.value < testData.value.length - 1) {
+				currentWordIndex.value++;
+			}
+		}
+
+		function doneTest() {
+			isFinishTest.value = true;
+			// Отправка данных.
+			// Отрисовка данных в виде графика, чо каво.
+		}
 
 		return {
 			isCreateTest,
 			modeTest,
-			countWordsInTest
+			countWordsInTest,
+			loading,
+			startTest,
+			currentWord,
+			currentWordIndex,
+			testData,
+			modeTestItems,
+			getWord,
+			nextWord,
+			doneTest,
+			isFinishTest,
 		}
 	},
 }
