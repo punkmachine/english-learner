cc93969653d5f2ef33826dd8fd3d11b58ee2ac54
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-01-04 07:03:24 +0600

    Переделал структуру store.

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 import { createStore } from 'vuex';
 
-import main from './main';
-import dictionary from './dictionary';
-import statistics from './statistics';
-import tests from './tests';
+import main from './modules/main';
+import dictionary from './modules/dictionary';
+import statistics from './modules/statistics';
+import tests from './modules/tests';
 
 export default createStore({
 	modules: {
