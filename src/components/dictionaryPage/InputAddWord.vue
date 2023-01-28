40c82be0576afa5668fd257f7bd03396e7dc325e
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2023-01-28 18:28:34 +0600

    upd: убрал не нужные паддинги у слота деталей инпута.

--- src/components/dictionaryPage/InputAddWord.vue
+++ src/components/dictionaryPage/InputAddWord.vue
@@ -1,19 +1,20 @@
 <template>
 	<v-text-field clearable>
 		<template #details>
-			<!-- TODO: убрать паддинги -->
 			<!-- TODO: сделать кнопку очистки -->
 			<!-- TODO: выровнять chip слева -->
 			<!-- TODO: удаление работает как-то странно -->
-			<v-chip-group column>
-				<Chip
-					v-for="(word, index) in wordsList"
-					:key="index"
-					@click:close="clickDeleteWord(index)"
-				>
-					{{ word }}
-				</Chip>
-			</v-chip-group>
+			<div class="pa-0">
+				<v-chip-group column>
+					<Chip
+						v-for="(word, index) in wordsList"
+						:key="index"
+						@click:close="clickDeleteWord(index)"
+					>
+						{{ word }}
+					</Chip>
+				</v-chip-group>
+			</div>
 		</template>
 	</v-text-field>
 </template>
