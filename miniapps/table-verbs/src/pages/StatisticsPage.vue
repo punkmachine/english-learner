9b24cf9cbb6ae5ceb1ceabf93e002083ca78e9f7
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-04-17 05:09:13 +0600

    add: базовая страница статистики

---
+++ miniapps/table-verbs/src/pages/StatisticsPage.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <h2 class="text-center text-xl font-bold mb-2">Статистика</h2>
+
+    <div>
+        <div class="grid grid-cols-2 gap-4">
+          <Card>
+            <template #title>
+              Осталось
+            </template>
+            <template #content>
+              <h3>{{ remaining }}</h3>
+            </template>
+          </Card>
+          <Card>
+            <template #title>
+              Пройдено
+            </template>
+            <template #content>
+              <h3>{{ completed }}</h3>
+            </template>
+          </Card>
+          <Card>
+            <template #title>
+              Успешно
+            </template>
+            <template #content>
+              <h3>{{ successful }}</h3>
+            </template>
+          </Card>
+          <Card>
+            <template #title>
+              Провалено
+            </template>
+            <template #content>
+              <h3>{{ failed }}</h3>
+            </template>
+          </Card>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue';
+import Card from 'primevue/card';
+
+const remaining = ref(+(localStorage.getItem('remaining-words') || 0)); // осталось
+const completed = ref(+(localStorage.getItem('completed-words') || 0)); // пройдено
+const successful = ref(+(localStorage.getItem('successful-words') || 0)); // успешно
+const failed = ref(+(localStorage.getItem('failed-words') || 0)); // провалено
+</script>
+
+<style scoped>
+
+</style>
--- miniapps/table-verbs/src/router/index.ts
+++ miniapps/table-verbs/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '@/pages/HomePage.vue';
+import StatisticsPage from '@/pages/StatisticsPage.vue';
 
 const router = createRouter({
   history: createWebHistory('/'),
@@ -8,6 +9,11 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomePage,
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      component: StatisticsPage,
     },
   ],
 });
